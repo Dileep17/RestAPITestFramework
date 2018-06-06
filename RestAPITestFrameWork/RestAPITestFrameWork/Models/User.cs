07565506a4b975070a4941c7c20462377272b145
Dileep B <dileep.srimani@gmail.com>
2018-06-06 11:20:53 +0530

    Dileep - Create UserData model, assert user endpoints response

---
+++ RestAPITestFrameWork/RestAPITestFrameWork/Models/User.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace RestAPITestFrameWork.Models
+{
+
+    class UserData
+    {
+        [JsonProperty("data")]
+        public User User { get; set; }
+
+        public UserData(int id, string firstName, string lastName, Uri avatar)
+        {
+            User = new User(id, firstName, lastName, avatar);
+        }
+
+        public bool Equals(UserData other)
+        {
+            return User.Equals((User)other.User);
+        }
+
+    }
+
+
+    class User
+    {
+        [JsonProperty("id")] public int Id { get; set; }
+
+        [JsonProperty("first_name")] public string FirstName { get; set; }
+
+        [JsonProperty("last_name")] public string LastName { get; set; }
+
+        [JsonProperty("avatar")] public Uri Avatar { get; set; }
+
+        public User(int id, string firstName, string lastName, Uri avatar)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Avatar = avatar;
+        }
+
+        protected bool Equals(User other)
+        {
+            return Id == other.Id && string.Equals(FirstName, other.FirstName) && string.Equals(LastName, other.LastName) && Equals(Avatar, other.Avatar);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((User) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Id;
+                hashCode = (hashCode * 397) ^ (FirstName != null ? FirstName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (LastName != null ? LastName.GetHashCode() : 0);
+                hashCode = (hashCode * 397) ^ (Avatar != null ? Avatar.GetHashCode() : 0);
+                return hashCode;
+            }
+        }
+    }
+}
---
+++ RestAPITestFrameWork/RestAPITestFrameWork/ServiceTests/UserTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using RestAPITestFrameWork.Models;
+using RestSharp;
+
+namespace RestAPITestFrameWork.ServiceTests
+{
+    [TestFixture]
+    public class UserTest
+    {
+        [Test]
+        public void SingleUserTest()
+        {
+            UserData expectedUserData = new UserData(2, "Janet", "Weaver", new Uri("https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg"));
+            var client = new RestClient("https://reqres.in/");
+            var request = new RestRequest("api/users/2", Method.GET);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+            UserData userData = JsonConvert.DeserializeObject<UserData>(response.Content);
+            Assert.IsTrue(userData.Equals(expectedUserData), "verifying exprcted user is same as actual user returned by API");
+            Console.WriteLine("heman");
+        }
+    }
+}
